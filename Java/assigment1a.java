223d52056aeb6dae197a8f3e9430a29e40e077b1
ANIKET SHARMA <71834668+ANIKETXO@users.noreply.github.com>
2024-02-22 17:12:12 +0530

    test
    gutfuygh

---
+++ Java/assigment1a.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class assigment1a {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = 
+    }
+}
--- Java/main.java
+++ Java/main.java
@@ -1,6 +1,11 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+import java.util.Scanner;
 
+public class main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter your roll number: ");
+        int rollno = input.nextInt();
+        System.out.println("here is your roll nimber: " + rollno);
+    }
 }
