36211d4b9cd141c6813b2de82fc89bc6917db907
ANIKET SHARMA <71834668+ANIKETXO@users.noreply.github.com>
2024-02-29 19:18:50 +0530

    update
    jsfhkas

--- Java/loops.java
+++ Java/loops.java
@@ -1,6 +1,6 @@
 public class loops {
     public static void main(String[] args) {
-        for (int num = 0; num < 10; num++) {
+        for (int num = 0; num < 10; num += 1) {
             System.out.println(num);
         }
     }
