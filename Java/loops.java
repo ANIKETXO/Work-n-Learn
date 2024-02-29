add6d017dc18d7c6b579f6d085c1a63e14a9d951
ANIKET SHARMA <71834668+ANIKETXO@users.noreply.github.com>
2024-02-29 21:32:59 +0530

    update
    lalalalalalalaa

--- Java/loops.java
+++ Java/loops.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class loops {
     public static void main(String[] args) {
-        for (int num = 0; num < 10; num += 1) {
-            System.out.println(num);
+        // for (int num = 0; num < 10; num += 1) {
+        // System.out.println(num);
+        // }
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int num = 1; num <= n; num++) {
+            System.out.println("Hello world!");
         }
     }
 }
