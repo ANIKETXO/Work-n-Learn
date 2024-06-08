7681edfb86249e045bdf3b833673bba92291772e
ANIKET SHARMA <71834668+ANIKETXO@users.noreply.github.com>
2024-06-08 14:11:03 +0530

    leraned

---
+++ C++/Pass by Reference and Value.cpp
@@ -0,0 +1,20 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> passedBy(int a, int &b) {
+      
+        a++;
+        b+=2;
+        return{a,b};
+        // code here
+    }
+};
