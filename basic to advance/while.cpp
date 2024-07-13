9925180750d2a596a16a520535c76fd30ca0e88c
ANIKET SHARMA <71834668+ANIKETXO@users.noreply.github.com>
2024-07-13 15:06:38 +0530

    while

---
+++ basic to advance/while.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    const int COUNT(10);
+    int i(0);
+    while(i<COUNT){
+        cout<<"I love C++"<<endl;
+        ++i;
+    }
+    return 0;
+}
