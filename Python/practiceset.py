cb03b63f46142387099eabc7c0b7e67e0f279906
ANIKET SHARMA <71834668+ANIKETXO@users.noreply.github.com>
2024-07-27 16:03:12 +0530

    practice

---
+++ Python/practiceset.py
@@ -0,0 +1,26 @@
+# a = 12
+# b = 18
+
+# c = a+b
+# print(c)
+
+# a = 34
+# b = 5
+
+# print("remainder when is divided by b is ",a%b)
+
+# a = input("Enter the value of a :")
+# print(type(a))
+
+# a = int(input("Enter the number 1: "))
+# b = int(input("Enter the number 2: "))
+# print("a is greater than b is", a>b)
+
+# a = int(input(" Enter the 1 number :"))
+# b = int(input(" Enter the 2 number :"))
+
+# print("The average of the two numbers is: ",(a+b)/2)
+
+a = int(input(" Enter your number :"))
+
+print("The square of the number is: ",a**2)
--- Python/variable.py
+++ Python/variable.py
@@ -1,3 +1,8 @@
 a = 1
 b = 2
 print(a+b)
+
+#logical operations
+
+e = True or False
+print(e)
